c33314a8c2e2fcab34e1aecb24044d6fa81fc954
Sebastian Jose Ferraro <sferraro@uns.edu.ar>
2025-06-23 09:58:58 +0200

    Add regression testing using pytest

---
+++ test_harm_osc.py
@@ -0,0 +1,31 @@
+import harm_osc_system_sympy as the_system  # Switch sympy and jax versions here
+import integrator
+import pytest
+
+
+# You can "simulate" replacing the sympy version with the jax version by
+# changing the import statement above.
+# If the tests were passing with the old sympy version, they should also
+# pass with the jax version.
+
+
+@pytest.mark.parametrize(
+    "phys_params",
+    [
+        ({"m": 1.0, "k": 1.0}),
+        ({"m": 2.0, "k": 1.0}),
+    ],
+)
+@pytest.mark.parametrize(
+    "h, N, init_cond",
+    [
+        (0.1, 100, [0.0, 0.0]),
+        (0.1, 20, [1.0, 1.1]),
+        (0.01, 100, [1.0, 1.1]),
+    ],
+)
+def test_harmonic_oscillator(phys_params, h, N, init_cond, ndarrays_regression):
+    trajectory = integrator.perform_simulation(
+        the_system.f_DEL_q2_first, phys_params, h, N, init_cond
+    )
+    ndarrays_regression.check({"values": trajectory})
